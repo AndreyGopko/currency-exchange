50d2600491a361cb99352a0e8f9043952c1d8f22
AndreyGopko <andy-gold@mail.ru>
2018-10-09 17:14:36 +0300

    init

---
+++ build/precache-manifest.cfa0d7cabc06150edb8877a314a5dfd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fc5018da2c56cbecaecd",
+    "url": "/static/js/main.fc5018da.chunk.js"
+  },
+  {
+    "revision": "7002853b81c5cfe74d59",
+    "url": "/static/js/1.7002853b.chunk.js"
+  },
+  {
+    "revision": "fc5018da2c56cbecaecd",
+    "url": "/static/css/main.21f97c23.chunk.css"
+  },
+  {
+    "revision": "7002853b81c5cfe74d59",
+    "url": "/static/css/1.4478d6b5.chunk.css"
+  },
+  {
+    "revision": "bbf598e9421b57c6b6e540b7f5b0553a",
+    "url": "/index.html"
+  }
+];
